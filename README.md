366fc889196d43ca1296606972db605538e114e0
mitblac <77762410+mitblac@users.noreply.github.com>
2021-02-04 00:38:47 -0500

    completes assignment

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # 01-Interactive-Fiction
-Project for MSCHC220
+Project 01 for Bl-MSCH-C220. 6 February 2021.
+
+Implements a Twine project exported as a JSON file in a homemade game engine written in Python.
+
+
+## Implementation
+Python 3.9.1 and Twine 2 (Twison 0.0.1 format)
+
+## References
+Interscope Records
+
+## Future Development
+N/A
+
+## Created by
+Mitchell Black
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import sys,os,json,re
+assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+def load(l):
+    f = open(os.path.join(sys.path[0], l))
+    data = f.read()
+    j = json.loads(data)
+    return j
+
+def find_passage(game_desc, pid):
+    for p in game_desc["passages"]:
+        if p["pid"] == pid:
+            return p
+    return{}
+
+
+# Removes Harlowe formatting from Twison description
+def format_passage(description):
+    description = re.sub(r'//([^/]*)//',r'\1',description)
+    description = re.sub(r"''([^']*)''",r'\1',description)
+    description = re.sub(r'~~([^~]*)~~',r'\1',description)
+    description = re.sub(r'\*\*([^\*]*)\*\*',r'\1',description)
+    description = re.sub(r'\*([^\*]*)\*',r'\1',description)
+    description = re.sub(r'\^\^([^\^]*)\^\^',r'\1',description)
+    description = re.sub(r'(\[\[[^\|]*?)\|([^\]]*?\]\])',r'\1->\2',description)
+    description = re.sub(r'\[\[([^(->\])]*?)->[^\]]*?\]\]',r'[ \1 ]',description)
+    description = re.sub(r'\[\[(.+?)\]\]',r'[ \1 ]',description)
+    return description
+
+
+
+def update(current,choice,game_desc):
+    if choice == "":
+        return current
+    for l in current["links"]:
+        if l["name"].lower() == choice:
+            current = find_passage(game_desc, l["pid"])
+            return current
+
+    print("Please try again.")
+    return current
+
+def render(current):
+    print("\n\n")
+#    print(current["name"]) <- I dont want it to print the name, so I'm commenting it out
+    print(format_passage(current["text"]))
+    print("\n")
+
+
+def get_input():
+    choice = input("")
+    choice = choice.lower().strip()
+    return choice
+
+
+
+
+def main():
+    game_desc = load("game.json")
+    current = find_passage(game_desc, game_desc["startnode"])
+    choice = ""
+
+    while choice != "quit" and current != {}:
+        current = update(current,choice,game_desc)
+        render(current)
+        choice = get_input()
+
+    print("\n")
+    print("Have a nice day.")
+    print("\n")
+
+
+
+if __name__ == "__main__":
+    main()
